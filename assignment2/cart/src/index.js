7faa63415e321a833666c775d5bf76b4e8b130e7
alhdhd1991 <82617069+alhdhd1991@users.noreply.github.com>
2023-04-12 15:02:31 -0500

    src

---
+++ assignment2/cart/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+import Shop from "./Shop";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Shop />
+  </React.StrictMode>
+);
